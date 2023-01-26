83a3713209edff538135fda948e0de813be965a0
Jose Mejia <jose.mejia04@gmail.com>
2023-01-26 09:36:31 -0500

    project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./Components/Header";
+import Nav from "./Components/Nav";
+import Article from "./Components/Article";
+import Footer from "./Components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App-body">
+      <Header />
+      <Nav />
+      <Article />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Article.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Article() {
+  return (
+    <section>
+      <article>
+        <time datetime="11-12-20">11/12/20</time>
+        <h2>On the Street in Brooklyn</h2>
+        <img src="/images/fashion-image-01.jpg" alt="fashion-image-01" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis,
+          quae sint. Maxime, voluptas animi! Recusandae minus iusto perspiciatis
+          molestiae quia aperiam, numquam at blanditiis obcaecati. Sit
+          doloremque fuga temporibus nesciunt.Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Debitis, quae sint. Maxime, voluptas
+          animi! Recusandae minus iusto perspiciatis molestiae quia aperiam,
+          numquam at blanditiis obcaecati. Sit doloremque fLorem ipsum dolor sit
+          amet consectetur, adipisicing elit. Debitis, quae sint. Maxime,
+          voluptas animi! Recusandae minus iusto perspiciatis molestiae quia
+          aperiam, numquam at blanditiis obcaecati. Sit doloremque fuga
+          temporibus nesciunt.uga temporibus nesciunt.
+        </p>
+        <aside>
+          <a href="#">Continues...</a>
+        </aside>
+      </article>
+
+      <article>
+        <time datetime="11-12-20">11/12/20</time>
+        <h2>Vintage in Vogue</h2>
+        <img src="/images/fashion-image-02.jpg" alt="fashion-image-02" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis,
+          quae sint. Maxime, voluptas animi! Recusandae minus iusto perspiciatis
+          molestiae quia aperiam, numquam at blanditiis obcaecati. Sit
+          doloremque fuga temporibus nesciunt.Lorem ipsum dolor sit amet
+          consectetur, adipisicing elit. Debitis, quae sint. Maxime, voluptas
+          animi! Recusandae minus iusto perspiciatis molestiae quia aperiam,
+          numquam at blanditiis obcaecati. Sit doloremque fLorem ipsum dolor sit
+          amet consectetur, adipisicing elit. Debitis, quae sint. Maxime,
+          voluptas animi! Recusandae minus iusto perspiciatis molestiae quia
+          aperiam, numquam at blanditiis obcaecati. Sit doloremque fuga
+          temporibus nesciunt.uga temporibus nesciunt.
+        </p>
+        <aside>
+          <a href="#">Continues...</a>
+        </aside>
+      </article>
+    </section>
+  );
+}
+
+export default Article;
